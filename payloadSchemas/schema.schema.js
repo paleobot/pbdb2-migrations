41bd095057635257a2388047f4add914a479e9a1
Douglas Meredith <noisyflowers@douglasmeredith.net>
2026-03-13 15:43:59 -0700

    Remove create/edit dichotomy to simplify schema.

--- payloadSchemas/schema.schema.js
+++ payloadSchemas/schema.schema.js
@@ -2,7 +2,87 @@
 Validation schemas in JSON Schema format. Note that fastify uses ajv (https://ajv.js.org/) for validation, which expects the schemas to be javascript objects rather than raw JSON. Consequently, property names (keys) do not require double quotes.
 */
 
-const schemaProperties = {
+export const schemaSchema = {
+    $schema: "https://json-schema.org/draft/2019-09/schema",
+    $id: "https://pbdb2.example.com/schemas/schema.json",
+    $defs: {
+        state: {
+            type: "object",
+            properties: {
+                name: {
+                    type: "string"
+                },
+                definition: {
+                    type: "string"
+                },
+                order: {
+                    type: "integer",
+                    minimum: 1
+                },
+                states: {
+                    type: "array",
+                    items: {
+                        $ref: "#/$defs/state"
+                    }
+                }
+            },
+            // quantitative conditional
+            if: {
+                properties: {
+                    name: {
+                        const: "quantity"
+                    }
+                }
+            },
+            then: {
+                properties: {
+                    value: {
+                        type: "string"
+                    }
+                },
+                required: ["value"]
+            }
+        },
+
+        character: {
+            type: "object",
+            properties: {
+                name: {
+                    type: "string"
+                },
+                definition: {
+                    type: "string"
+                },
+                order: {
+                    type: "integer",
+                    minimum: 1
+                },
+                states: {
+                    type: "array",
+                    items: {
+                        $ref: "#/$defs/state"
+                    }
+                },
+                characters: {
+                    type: "array",
+                    items: {
+                        $ref: "#/$defs/character"
+                    }
+                }
+            }
+        }
+    },
+
+    title: "Schema",
+    description: "A schema payload in the PBDB database",
+    type: "object",
+    unevaluatedProperties: false, //new with Draft 2019-09
+    required: [
+        "title",
+        "year",
+        "schemaDefinition"
+    ],
+    properties: {
         legacyIDs: {
             type: "object",
             properties: {
@@ -89,94 +169,7 @@ const schemaProperties = {
                 }
             }
         }
-
+    }
 };
 
-export const createSchema = {
-	tags:["Schema"],
-	hide: true,
 
-    $defs: {
-        state: {
-            type: "object",
-            properties: {
-                name: {
-                    type: "string"
-                },
-                definition: {
-                    type: "string"
-                },
-                order: {
-                    type: "integer",
-                    minimum: 1
-                },
-                states: {
-                    type: "array",
-                    items: {
-                        $ref: "#/$defs/state"
-                    }
-                }
-            },
-            // quantitative conditional
-            if: {
-                properties: {
-                    name: {
-                        const: "quantity"
-                    }
-                }
-            },
-            then: {
-                properties: {
-                    value: {
-                        type: "string"
-                    }
-                },
-                required: ["value"]
-            }
-        },
-
-        character: {
-            type: "object",
-            properties: {
-                name: {
-                    type: "string"
-                },
-                definition: {
-                    type: "string"
-                },
-                order: {
-                    type: "integer",
-                    minimum: 1
-                },
-                states: {
-                    type: "array",
-                    items: {
-                        $ref: "#/$defs/state"
-                    }
-                },
-                characters: {
-                    type: "array",
-                    items: {
-                        $ref: "#/$defs/character"
-                    }
-                }
-            }
-        }
-    },
-
-    body: {
-		type: 'object',
-		properties: {
-			schema: {
-				type: "object",
-				properties: schemaProperties,
-				unevaluatedProperties: false, //new with Draft 2019-09
-                required: [
-                    "title",
-                    "year",
-                    "schemaDefinition"
-                ]
-            }
-        }
-    }
-}
