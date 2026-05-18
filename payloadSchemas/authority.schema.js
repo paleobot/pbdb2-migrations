35e8832fd6afc4164638d692807cdef84d4e0e1c
Douglas Meredith <noisyflowers@douglasmeredith.net>
2026-05-18 16:48:58 -0700

    New authority schema

---
+++ payloadSchemas/authority.schema.js
@@ -0,0 +1,59 @@
+/*
+Validation schemas in JSON Schema format. Note that fastify uses ajv (https://ajv.js.org/) for validation, which expects the schemas to be javascript objects rather than raw JSON. Consequently, property names (keys) do not require double quotes.
+*/
+
+export const authoritySchema = {
+    $schema: "https://json-schema.org/draft/2019-09/schema",
+    $id: "https://pbdb2.example.com/schemas/authority.json",
+    title: "Authority",
+    description: "An authority payload in the PBDB database",
+    type: "object",
+    unevaluatedProperties: false, //new with Draft 2019-09
+    properties: {
+        authority: {
+            type: "object",
+            properties: {
+                legacyIDs: {
+                    type: "object",
+                    properties: {
+                        oldpbdbID: {
+                            type: "string",
+                            description: "Legacy ID for authorities migrated from old PBDB"
+                        },
+                    }
+                },
+                authors: {
+                    type: "array",
+                    minItems: 1,
+                    items: {
+                        type: "object",
+                        properties: {
+                            familyName: {type: "string"},
+                            givenName: {type: "string"}
+                        }
+                    }
+                },
+                year: {
+                    type: "string",
+                    maxLength: 4
+                },
+                referenceIsAuthority: {
+                    type: "boolean"
+                }
+            },
+            unevaluatedProperties: false, //new with Draft 2019-09
+            required: [
+                "referenceIsAuthority",
+            ],
+            if: {
+                properties: { referenceIsAuthority: { const: false } }
+            },
+            then: {
+                required: ["authors"]
+            }
+            else: {
+                not: { required: ["authors"] }
+            }
+        }
+    }
+}
